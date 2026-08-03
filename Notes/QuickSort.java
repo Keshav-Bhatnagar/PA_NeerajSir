d70edc37f87699fefda78f310ad03939daa07671
Keshav Bhatnagar <keshavbhatnagar500@gmail.com>
2026-08-03 17:37:57 +0530

    QuickSort
    Source: https://www.programiz.com/java-programming/online-compiler/
    Timestamp: 8/3/2026, 5:37:57 PM

---
+++ Notes/QuickSort.java
@@ -0,0 +1,52 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class QuickSort {
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7,2,9,1,5,8,3};
+
+        System.out.print("Before Sorting: ");
+        printArray(arr);
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.print("After Sorting: ");
+        printArray(arr);
+    }
+}
