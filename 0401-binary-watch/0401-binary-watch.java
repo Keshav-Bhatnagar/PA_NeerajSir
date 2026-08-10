a3a30aa18407d421ca89663b3b7c7ab8f0847757
Keshav Bhatnagar <keshavbhatnagar500@gmail.com>
2026-08-10 17:28:16 +0530

    Time: 13 ms (13.11%), Space: 49.6 MB (10.44%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+
+    public void backtrack(int index, int count, int turnedOn,
+                           int[] leds, List<String> answer) {
+
+        if (count == turnedOn) {
+
+            int hour = 0;
+            int minute = 0;
+
+            for (int i = 0; i < 4; i++) {
+                if (leds[i] == 1) {
+                    hour += (1 << i);
+                }
+            }
+
+            for (int i = 4; i < 10; i++) {
+                if (leds[i] == 1) {
+                    minute += (1 << (i - 4));
+                }
+            }
+
+            if (hour <= 11 && minute <= 59) {
+                String time = hour + ":" + String.format("%02d", minute);
+                answer.add(time);
+            }
+
+            return;
+        }
+
+        if (index == 10) {
+            return;
+        }
+
+        leds[index] = 1;
+        backtrack(index + 1, count + 1, turnedOn, leds, answer);
+
+        // Turn OFF this LED
+        leds[index] = 0;
+        backtrack(index + 1, count, turnedOn, leds, answer);
+    }
+
+    public List<String> readBinaryWatch(int turnedOn) {
+
+        List<String> answer = new ArrayList<>();
+
+        if (turnedOn > 8) {
+            return answer;
+        }
+
+        int[] leds = new int[10];
+
+        backtrack(0, 0, turnedOn, leds, answer);
+
+        return answer;
+    }
+}
