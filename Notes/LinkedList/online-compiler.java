1d6a5290ff7e1c49d86067b7eff777db137a3744
Keshav Bhatnagar <keshavbhatnagar500@gmail.com>
2026-08-17 17:08:55 +0530

    Concept Of LinkedList
    Source: https://www.programiz.com/java-programming/online-compiler/
    Timestamp: 8/17/2026, 5:08:54 PM

---
+++ Notes/LinkedList/online-compiler.java
@@ -0,0 +1,32 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        this.data = d;
+        this.next = null;
+    }
+}
+class Main {
+    public static void Print(Node root) {
+        if(root == null ) return ;
+        while(root!=null) {
+            System.out.print(root.data +" ");
+            root = root.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        Node temp = head;
+        for(int i=4;i>=0;i--) {
+            temp.next = new Node(i);
+            temp=temp.next;
+        }
+        Print(head);
+
+
+    }
+}
